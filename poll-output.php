73cf01e7e6e0b510aae0cb210d67cc0ac9deb7ae
Ian Newton <i.newton@open.ac.uk>
2011-07-26 11:31:58 +0100

    missing file poll-output added

---
+++ poll-output.php
@@ -0,0 +1,42 @@
+<?PHP
+/*========================================================================================*\
+	#	Coder    :  Ian Newton
+	#	Date     :  24th May,2011
+	#	Test version  
+	#	controller to process actions queued in the media_actions table and report status to the admin server
+\*=========================================================================================*/
+
+require_once("lib/config.php");
+require_once("lib/classes/polling.class.php");
+
+// Initialise objects
+	$mysqli = new mysqli($dbLogin['dbhost'], $dbLogin['dbusername'], $dbLogin['dbuserpass'], $dbLogin['dbname']);
+	$pollObj = new Default_Model_Polling_Class($mysqli);	
+
+	if (isset($_REQUEST['time']) && $_REQUEST['time']>=1) $time = $_REQUEST['time']; else $time=1;
+	if (isset($_REQUEST['number']) && $_REQUEST['number']>=1) $number = $_REQUEST['number']; else $number=1;
+
+// Poll for any completed or failed commands on Media and Encoder queues
+
+	for ( $i = 1; $i <= $number; $i++) {
+
+		$files = $pollObj->read_folder_directory($source['encoder']);
+		
+		if (is_array($files))
+		{
+//			print_r($files);
+			$pollObj->check_folder_directory($files);
+			
+		} else {
+			$i = $number;	
+		}
+		
+		$pollObj->spawn_or_update_commands();
+
+// sleep for n seconds
+		flush();
+//		echo $i." - ";
+		sleep($_REQUEST['time']);
+	}
+
+?>
